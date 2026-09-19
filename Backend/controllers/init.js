74b8e8791af77db51e18018106881ef1f44bdbbe
yash-kumawat-0 <kumawatyash378@gmail.com>
2026-09-19 16:31:13 +0530

    Created init.js

--- Backend/controllers/init.js
+++ Backend/controllers/init.js
@@ -1,5 +1,21 @@
-async function initRepo(){
-    console.log("Init is called")
+const fs = require("fs").promises;
+const path = require("path");
+
+async function initRepo() {
+  const repoPath = path.resolve(process.cwd(), ".mygit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET }),
+    );
+    console.log("Repository Initialized!")
+  } catch (err) {
+    console.error("Error Initializing Repository: ", err);
+  }
 }
 
-module.exports = {initRepo};
+module.exports = { initRepo };
