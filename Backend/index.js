23a0004bf2a204f63898e81dd8da243c9bf8281a
yash-kumawat-0 <kumawatyash378@gmail.com>
2026-09-25 16:32:17 +0530

    Created Server

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,13 @@
-const yargs = require("yargs")
-const { hideBin } = require("yargs/helpers")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 const { initRepo } = require("./controllers/init");
 const { addRepo } = require("./controllers/add");
 const { commitRepo } = require("./controllers/commit");
@@ -7,37 +15,106 @@ const { pushRepo } = require("./controllers/push");
 const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
 
-yargs(hideBin(process.argv))
-    .command("start", "Start a Server", {}, startServer)
-    .command("init", "Initialize a new Repository", {}, initRepo)
-    .command("add <file>", "Add a file to the Repository", (yargs)=>{
-        yargs.positional("file", {
-            describe:"File to added to the staging area",
-            type: "string"
-        })
-    }, (argv)=>{
-        addRepo(argv.file);
-    })
-    .command("commit <message>", "Commit the staged file", (yargs)=>{
-        yargs.positional("message",{
-            describe: "Commit Message",
-            type: "string"
-        })
-    }, (argv)=>{
-        commitRepo(argv.message)
-    })
-    .command("push", "Push commits to S3", {}, pushRepo)
-    .command("pull", "Pull commits to S3", {}, pullRepo)
-    .command("revert <commitID>", "Revert to the specific commit",(yargs)=>{
-        yargs.positional("commitID", {
-            describe: "commit ID to revert to",
-            type: "string"
-        })
-    }, (argv)=>{
-        revertRepo(argv.commitID)
-    })
-    .demandCommand(1, "You need at least one command").help().argv;
+dotenv.config();
 
-function startServer(){
-    console.log("Server Logic Called");
+yargs(hideBin(process.argv))
+  .command("start", "Start a Server", {}, startServer)
+  .command("init", "Initialize a new Repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "Add a file to the Repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to added to the staging area",
+        type: "string",
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    },
+  )
+  .command(
+    "commit <message>",
+    "Commit the staged file",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit Message",
+        type: "string",
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    },
+  )
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits to S3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert to the specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "commit ID to revert to",
+        type: "string",
+      });
+    },
+    (argv) => {
+      revertRepo(argv.commitID);
+    },
+  )
+  .demandCommand(1, "You need at least one command")
+  .help().argv;
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGODB_URI;
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("MONGODB CONNECTED");
+    })
+    .catch((err) => {
+      console.error("UNABLE TO CONNECT : ", err);
+    });
+
+  app.use(cors({ origin: "*" }));
+
+  app.get("/", (req, res) => {
+    res.send("WELCOME");
+  });
+
+  const user = "test";
+
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket)=>{
+    socket.on("joinRoom", (userID)=>{
+        user = userID;
+        console.log("=========");
+        console.log(user);
+        console.log("=========");
+        socket.join(user);
+    });
+  });
+
+  const db = mongoose.connection;
+
+  db.once("open", async ()=>{
+    console.log("CRUD operations are called.");
+  })
+
+  httpServer.listen(port, ()=>{
+    console.log(`SERVER IS RUNNING ON PORT: ${port}`)
+  })
 }
